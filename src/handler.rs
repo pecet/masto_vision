6464e2e197c29ffc884c9a99e765b070b46ac2c2
Piotr Czarny <926143+pecet@users.noreply.github.com>
2023-11-10 18:39:13 +0100

    Handle adding images

--- src/handler.rs
+++ src/handler.rs
@@ -4,6 +4,7 @@ use crate::{vision::Vision, config::Config};
 use chrono::Local;
 
 use futures_util::{TryFutureExt, TryStreamExt};
+use kv_log_macro::warn;
 use log::{LevelFilter, debug, info, error};
 use mastodon_async::{Mastodon, prelude::*};
 
@@ -44,29 +45,43 @@ impl Handler {
         let mastodon = Mastodon::from(data);
         log::info!("Logging in to Mastodon");
         let you = mastodon.verify_credentials().await?;
+        let user_id = &format!("{}", &you.id);
         log::info!("Logged in");
         log::debug!("Logged in as user id: {}", you.id);
         log::debug!("Waiting for mastodon events");
         let stream = mastodon.stream_user().await?;
         stream
             .try_for_each(|(event, _client)| async move {
+                let user_id = user_id.clone();
                 debug!("Event received:\n{:#?}", &event);
                 match event {
                     Event::Update(update) => {
                         debug!("Update event received:\n{:#?}", &update);
-                        update.media_attachments.iter().cloned().for_each(|attachment| {
-                            debug!("Attachment received:\n{:#?}", &attachment);
-                            if attachment.media_type == MediaType::Image
-                                // unwrap is safe since is not none
-                                && (attachment.description.is_none() || attachment.description.as_ref().unwrap().is_empty())  {
-                                debug!("Attachment {} has no description", &attachment.id);
+                        if format!("{}", update.account.id) == user_id {
+                            update.media_attachments.iter().cloned().for_each(|attachment| {
+                                debug!("Attachment received:\n{:#?}", &attachment);
+                                if attachment.media_type == MediaType::Image
+                                    // unwrap is safe since is not none
+                                    && (attachment.description.is_none() || attachment.description.as_ref().unwrap().is_empty())  {
+                                    debug!("Attachment {} has no description", &attachment.id);
 
-                                // create new thread and run it in background
-                                tokio::spawn(async move {
-                                    info!("Generating description for attachment {}", attachment.id);
-                                });
-                            }
-                        });
+                                    // create new thread and run it in background
+                                    tokio::spawn(async move {
+                                        if let Some(url) = attachment.url.clone() {
+                                            debug!("Generating description for attachment {} with URL: {}", attachment.id, attachment.url.unwrap());
+                                            let description = Vision().get_description(url).await.unwrap_or_else(|err| {
+                                                error!("Failed to generate description for attachment {}: {:#?}", attachment.id, err);
+                                                "".to_string()
+                                            });
+                                            info!("Generated description for attachment {}: {}", attachment.id, description);
+                                        } else {
+                                            warn!("Cannot get URL for attachment {}", attachment.id);
+                                        }
+
+                                    });
+                                }
+                            });
+                        }
                     }
                     Event::Notification(_) => {},
                     Event::Delete(_) => {},
